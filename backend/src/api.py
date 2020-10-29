59761c1d2af9036c1dbbff7bc9e6461d2c3a84a7
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-10-29 18:51:42 +0200

    Edit drinks

--- backend/src/api.py
+++ backend/src/api.py
@@ -63,17 +63,30 @@ def add_new_drink(jwt):
         })
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def edit_drink(jwt, drink_id):
+    drink = Drink.query.get(drink_id)
+
+    if drink is None:
+        abort(404)
+
+    data = request.get_json()
+
+    if 'title' in data:
+        drink.title = data.get('title', None)
+
+    if 'recipe' in data:
+        drink.recipe = json.dumps(data.get('recipe', None))
+
+    drink.update()
+
+    edited_drink = Drink.query.filter_by(id=drink_id).first()
+
+    return jsonify({
+        'success': True,
+        'drinks': [edited_drink.long()]
+        })
 
 
 '''
